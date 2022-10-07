2805281e8c535675d62aa9351f382024ae86560f
Bassem <bassemsaeed620@gmail.com>
2022-10-07 17:35:04 +0200

    1000

--- server.js
+++ server.js
@@ -11,15 +11,11 @@ const port = process.env.PORT || 5000
 connectDB();
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended: false}));
-
 app.use('/api/admins', require("./routes/adminRoutes"));
 app.use('/api/courses', require("./routes/courseRoutes"));
 app.use('/api/students', require("./routes/studentRoutes"));
-
 app.use(errorHandler);
-
 app.listen(port, _ => console.log(`Server started on port: ${port}` .cyan));
